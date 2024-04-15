9eb3b90bc7b55cd78e6e5bd1f3129bffd70c5c59
RupamNUC <rupam.sarkar@outlook.in>
2024-04-15 20:50:16 +0530

    Initial Commit

---
+++ src/main/java/com/payroll/springapi/SpringApiApplication.java
@@ -0,0 +1,14 @@
+package com.payroll.springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiApplication.class, args);
+	}
+
+}
