2e7ccb08f7df15684ef3274fbf625c1e336f2210
RupamNUC <rupam.sarkar@outlook.in>
2024-04-21 17:35:23 +0530

    done

--- src/main/java/com/payroll/springapi/securityConfigurations/SecurityConfiguration.java
+++ src/main/java/com/payroll/springapi/securityConfigurations/SecurityConfiguration.java
@@ -54,6 +54,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(WHITE_LIST_URL)
                                 .permitAll()
+                                .requestMatchers(GET, "/api/public/credentials/**").hasAnyAuthority("O")
 //                                .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
 //                                .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
 //                                .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
@@ -62,6 +63,8 @@ public class SecurityConfiguration {
                                 .anyRequest()
                                 .authenticated()
                 )
+                /*.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
+                    .permitAll())*/
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
